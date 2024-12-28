ae0bd3926e0da57d23028f6619f7bef8d58a4d11
sabrinaeshmurodova <sabrinaeshmurodova8@gmail.com>
2024-12-28 23:46:16 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,35 +11,35 @@ st.title("Heart Disease Predictor")
 
 GENDER = st.number_input("GENDER", min_value=0, max_value=120, value=30)
 AGE = st.selectbox("AGE", options=[0, 1], format_func=lambda x: "Female" if x == 0 else "Male")
-SMOKING = st.selectbox("Chest Pain Type", options=[1,2], format_func=lambda x: ["chekadi", "chekmaydi", "Non-anginal][x])
-trestbps = st.number_input("Resting Blood Pressure (mm Hg)", min_value=0, max_value=300, value=120)
-chol = st.number_input("Serum Cholesterol (mg/dl)", min_value=0, max_value=600, value=200)
+SMOKING = st.selectbox("Chest Pain Type", options=[1,2], format_func=lambda x: ["chekadi", "chekmaydi", "Non-anginal][x]")
+SMOKING = st.selectbox("Resting Electrocardiographic Results", options=[0, 1], format_func=lambda x: ["Normal", "ST-T wave abnormality"][x])
 YELLOW_FINGERS = st.selectbox("Fasting Blood Sugar > 120 mg/dl", options=[0, 1], format_func=lambda x: "False" if x == 0 else "True")
 ANXIETY = st.selectbox("Resting Electrocardiographic Results", options=[0, 1], format_func=lambda x: ["Normal", "ST-T wave abnormality"][x])
-PEER_PRESSURE = st.selectboxput("Maximum Heart Rate Achieved", , options=[0, 1], format_func=lambda x: ["Normal", "ST-T wave abnormality"][x])
-CHRONIC DISEASE	 = st.selectbox("Exercise-CHRONIC DISEASE	", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
+PEER_PRESSURE = st.selectboxput("Maximum Heart Rate Achieved",options=[0, 1], format_func=lambda x: ["Normal", "ST-T wave abnormality"][x])
+CHRONIC_DISEASE	 = st.selectbox("Exercise-CHRONIC DISEAS", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
 WHEEZINGk = st.selectbox("ST Depression", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
-ALCOHOL CONSUMING = st.selectbox("Slope of the Peak Exercise ST Segment", options=[0, 1], format_func=lambda x: ["Upsloping", "Flat"][x])
+ALCOHOL_NSUMING = st.selectbox("Slope of the Peak Exercise ST Segment", options=[0, 1], format_func=lambda x: ["Upsloping", "Flat"][x])
+WHEEZING = st.selectbox("ST Depression", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
+ALCOHOL_CONSUMING = st.selectbox("Slope of the Peak Exercise ST Segment", options=[0, 1], format_func=lambda x: ["Upsloping", "Flat"][x])
 COUGHING = st.selectbox("Number of Major Vessels Colored by Fluoroscopy", options=[0, 1])
-SHORTNESS OF BREATH = st.selectbox("Thalassemia", options=[1, 2], format_func=lambda x: ["Normal", "Fixed defect"][x - 1])
-SWALLOWING DIFFICULTY = st.selectbox("ST Depression", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
-CHEST PAIN = st.selectbox("ST Depression", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
+SHORTNESS_OF_BREATH = st.selectbox("Thalassemia", options=[1, 2], format_func=lambda x: ["Normal", "Fixed defect"][x - 1])
+SHORTNESS_OF_BREATH = st.selectbox("Thalassemia", options=[0, 1], format_func=lambda x: ["Normal", "Fixed defect"][x - 1])
+SWALLOWING_DIFFICULTY = st.selectbox("ST Depression", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
+CHEST_PAIN = st.selectbox("ST Depression", options=[0, 1], format_func=lambda x: "No" if x == 0 else "Yes")
 
 if st.button("Predict Heart Disease"):
     user_input = [
-        AGE, SMOKING, YELLOW_FINGERS, ANXIETY, PEER_PRESSURE, CHRONIC DISEASE, WHEEZING, ALCOHOL CONSUMING, COUGHING, SHORTNESS OF BREATH, SWALLOWING DIFFICULTY, CHEST PAIN
-    ]
+        AGE, SMOKING, YELLOW_FINGERS, ANXIETY, PEER_PRESSURE, CHRONIC_DISEASE, WHEEZING, ALCOHOL_CONSUMING, COUGHING, SHORTNESS_OFBREATH, SWALLOWING_DIFFICULTY, CHEST_PAIN
 
-   
     features = [
-        "AGE", "SMOKING", "YELLOW_FINGERS", "ANXIETY", "PEER_PRESSURE","CHRONIC DISEASE", "WHEEZING", "ALCOHOL CONSUMING", "COUGHING","SHORTNESS OF BREATH", "SWALLOWING DIFFICULTY", "CHEST PAIN"
+        "AGE", "SMOKING", "YELLOW_FINGERS", "ANXIETY", "PEER_PRESSURE","CHRONIC_DISEASE", "WHEEZING", "ALCOHOL_CONSUMING", "COUGHING","SHORTNESS_OF_BREATH", "SWALLOWING_DIFFICULTY", "CHEST_PAIN"
     ]
     input_df = pd.DataFrame([user_input], columns=features)
+    input_df = pd.DataFrame( columns=features)
 
- 
     result = model.predict(input_df)
 
- 
+
     if result[0] == 1:
         st.warning("Prognoz: o`pka saraton kasalligi xavfi yuqori!")
     else:
